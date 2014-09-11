5141d91e95dc1f369ffd63317f8b560de206770f
Nayan Seth <nayantronix@gmail.com>
2014-09-11 09:29:23 +0530

    Deleting files

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$currency = '₹';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'bolt';
-$db_host = 'localhost';
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
-?>
