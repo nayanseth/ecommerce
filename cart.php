c79cca3a3ee0950d5528a5f8e84994edcb042db5
Ayush Kumar Soni <71806100+ayushkumarsoni@users.noreply.github.com>
2020-10-01 12:18:51 +0530

    For space can show error on session creation in live server

--- cart.php
+++ cart.php
@@ -1,11 +1,7 @@
 <?php
-
 //if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
 if(session_id() == '' || !isset($_SESSION)){session_start();}
-
 include 'config.php';
-
-
 ?>
 
 <!doctype html>
